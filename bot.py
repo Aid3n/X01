766191b84713fad2604c880423a9fdb3ba550e54
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-08-13 01:46:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import discord
 from discord.ext import commands
 
-TOKEN = 'NDU1NzI1MTY4NTU2MjQ1MDAz.DlIKAw.TSNkW0vnVMtfJUnXne6ZWiC39cA'
-
 client = commands.Bot(command_prefix = '.')
 
 @client.event
