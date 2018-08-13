9939bda33b4728559fb8ee18320d57f3c488510c
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-08-13 03:01:14 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,15 +15,5 @@ async def on_message(message):
     channel = message.channel
     if message.content.startswith('.version'):
         await client.send_message(channel, '0.2.0')
-
-@client.command(pass_context=True)
-async def clear(ctx, amount=100):
-    channel = ctx.message.channel
-    messages = []
-    async for message in client.logs_from(channel, limit=int(amount) + 1):
-        messages.append(messages)
-    await client.delete_messages(messages)
-    await client.say('Mensajes borrados.')
-        
       
 client.run(os.environ['BOT_TOKEN'])
